97ee8ec555f835959e5d3974c1856133acec3afd
ad222kr <ad222kr@student.lnu.se>
2016-04-04 15:45:26 +0100

    popup works

--- app/components/map/controllers/mapController.js
+++ app/components/map/controllers/mapController.js
@@ -5,27 +5,37 @@ angular
 MapController.$inject = ["NgMap", "PubService", "$scope"]
 function MapController(NgMap, PubService, $scope) {
   var vm = this;
+  var markers = [];
+  var infoWindow;
+
   NgMap.getMap().then(function(map) {
     vm.map = map;
+    PubService.getPubs()
+      .then(function(data) {
+        infoWindow = new google.maps.InfoWindow();
+        map.addListener("click", closeInfoWindow);
+        data.pubs.forEach(function(pub) {
+          var marker = new google.maps.Marker({position: { lat: pub.position.latitude, lng: pub.position.longitude }, map: map})
+          marker.addListener("click", function() {
+            openInfoWindow(marker, pub);
+          })
+        });
+      })
+      .catch(function(error) {
+        console.log("Error: " + error)
+      });
   });
 
-  var pubPromise = PubService.getPubs();
+  function openInfoWindow(marker, pub) {
+    var latLng = marker.getPosition();
+    infoWindow.setContent("<b>" + pub.name + "</b>");
+    infoWindow.open(vm.map, marker);
 
-  pubPromise
-    .then(function(data) {
-      console.log(data.pubs);
-      vm.pubs = data.pubs;
-    })
-    .catch(function(error) {
-      console.log("Error: " + error)
-    });
+  }
 
-  $scope.$on("mapInitialized", function(evt, evtMap) {
-    vm.showInfo = function(e) {
-      console.log(e.latLng);
-      console.log("YEEEAH BABY")
-    }
-  });
-  return vm;
+  function closeInfoWindow() {
+    infoWindow.close();
+  }
+
 
 }
